ed0a17ba9e9291e5419232bff1ae780df1bebf65
9416391321\!aB <ankitbaghel13@gmail.com>
2017-10-02 22:25:26 +0530

    Adding src/pack/Example.java || adding src package

---
+++ src/pack/Example.java
@@ -0,0 +1,28 @@
+package pack;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Example {
+
+	public static void main(String[] args) {
+
+		/*
+		 * Pattern p = Pattern.compile(".s"); Matcher m = p.matcher("as");
+		 * boolean b = m.matches();
+		 */
+
+		/* boolean b = Pattern.compile(".s").matcher("as").matches(); */
+
+		// boolean b = Pattern.matches(".s", "as");
+		/*boolean b = Pattern.matches("[abc]", "b");
+
+		System.out.println(b);*/
+
+		for(int i=1;i<=10;i++){
+			System.out.println("hi no -"+i);
+		}
+		System.out.println("But you didn't reply :(");
+	}
+
+}
---
+++ src/pack/shfjkds.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package pack;
+
+/**
+ * @author Ankit
+ *
+ */
+public final class shfjkds extends Example {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
